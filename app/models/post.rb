9c4fefa529b8aa69aa0d2f6be29f068026167076
Piyush Lalwani <piyushlalwani143@gmail.com>
2024-07-08 17:27:55 +0530

    Modify models and extending associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  has_many :likes
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :likes
+    has_many :comments
+    has_and_belongs_to_many :groups
 end
